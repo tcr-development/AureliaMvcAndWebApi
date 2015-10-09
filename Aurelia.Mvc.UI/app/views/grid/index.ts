f8305747635dd8bcdcc8cf97345474d61456fe24
John Rigsby <jrigsby@tcrdev.com>
2015-10-08 23:06:47 -0400

    alter how to observe changes

--- Aurelia.Mvc.UI/app/views/grid/index.ts
+++ Aurelia.Mvc.UI/app/views/grid/index.ts
@@ -8,7 +8,6 @@ import Wijmogrid = require("wijmogrid");
 
 export class Search{
     static inject() { return [Observer.MultiObserver]; }
-   // @bindable grid1: any;
     dispose: () => void;
     multiObserver;
     grid1: Wijmogrid.WijmoGrid;
@@ -20,28 +19,30 @@ export class Search{
    }
 
     activate() {
-        var self = this;
-        self.dispose = self.multiObserver.observe(
-            [[self.grid1State, 'activated']],
-            () => self.observeGrid());
+        //var self = this;
+        //self.dispose = self.multiObserver.observe(
+        //    [[self.grid1State, 'activated']],
+        //    () => self.observeGrid());
     }
 
+    attached() {
+        var self = this;
+        self.dispose = self.multiObserver.observe(
+            [[self.grid1.selectedRows, 'length']],
+            () => self.updateCount());
+    }
 
-   selectedRowsChanged() {
-       //this.$parent.count = this.selectedRows.length;
-   }
+   //observeGrid() {
+   //    if (!this.grid1) {
+   //        return 0;
+   //    }
+   //    var self = this;
+   //    self.dispose = self.multiObserver.observe(
+   //        [[self.grid1.selectedRows, 'length']],
+   //        () => self.updateCount());
 
-   observeGrid() {
-       if (!this.grid1) {
-           return 0;
-       }
-       var self = this;
-       self.dispose = self.multiObserver.observe(
-           [[self.grid1.selectedRows, 'length']],
-           () => self.updateCount());
-
-       this.updateCount();
-   }
+   //    this.updateCount();
+   //}
 
    updateCount() {
        if (!this.grid1) {
